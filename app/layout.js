3b14c7702b03fdbf593724510415b36522bada9e
Saleh Ahmed <44835364+SalehAhmed10@users.noreply.github.com>
2023-02-27 07:45:36 +0500

    added-loading set-theme

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,32 @@
 "use client"
 import { Montserrat } from '@next/font/google'
-import { ChakraProvider, Container, extendBaseTheme, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, Container, extendBaseTheme, extendTheme, } from '@chakra-ui/react'
 import SidebarWithHeader from '@/Components/SidebarWithHeader'
 import './globals.css'
 import CountProvider from '@/context/Count'
 import ConstantsProvider from '@/context/constants'
+import { Suspense } from 'react'
+
+import Loading from './Loading'
+import { mode } from '@chakra-ui/theme-tools'
+import { ColorMode } from '@chakra-ui/react'
+import { extendTheme as extendThemeChakra } from '@chakra-ui/react'
+import { globalStyles, fonts, colors, config } from './Theme'
 
 
-const montserrat = Montserrat({
-  weights: [400, 500, 600, 700],
-  subsets: ['latin-ext'],
 
-})
+// const montserrat = Montserrat({
+//   weights: [400, 500, 600, 700],
+//   subsets: ['latin-ext'],
+
+// }
+
 
 const theme = extendTheme({
-  colors: {
-    primary: '#201D29',
-  },
-  // breakpoints: {
-  //   sm: '414px',
-  //   md: '584px',
-  //   lg: '1280px',
-  // },
-});
+  styles: globalStyles
+  , fonts, colors, config
+
+})
 
 
 export default function RootLayout({ children }) {
@@ -34,18 +38,23 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`${montserrat.className}`}>
-        <ConstantsProvider>
-          <CountProvider>
-            <ChakraProvider theme={theme} >
+      {/* className={`${montserrat.className}`} */}
+      <body >
+        <ChakraProvider
+          theme={theme}
+          resetCSS={true}
+        >
+          <ConstantsProvider>
+            <CountProvider>
               <SidebarWithHeader>
+                <Suspense fallback={<Loading />}>
 
-                {children}
-
+                  {children}
+                </Suspense>
               </SidebarWithHeader>
-            </ChakraProvider>
-          </CountProvider>
-        </ConstantsProvider>
+            </CountProvider>
+          </ConstantsProvider>
+        </ChakraProvider>
       </body>
     </html>
   )
